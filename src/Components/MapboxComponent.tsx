428dd5b850b8d4b7291125896987478a8a1a7864
tejabalu <tejabalu15@gmail.com>
2022-09-19 00:04:51 -0700

    initial commit, test with mapbox-react wrapper

---
+++ src/Components/MapboxComponent.tsx
@@ -0,0 +1,95 @@
+import * as React from "react";
+import { useEffect, useMemo, useState } from "react";
+import MapGL, { Layer, Source } from "react-map-gl";
+import ControlPanel from "./control-panel";
+import { circleLayer, heatmapLayer } from "./map-style";
+
+const MAPBOX_TOKEN =
+  "pk.eyJ1IjoidGVqYWJhbHUiLCJhIjoiY2w4N29tb215MWJnYzN1cG5qYzFsZ29sZyJ9.xb_GFzh_Dv7-tB5QWqpPlw"; // Set your mapbox token here
+
+function filterFeaturesByDay(featureCollection, time) {
+  const date = new Date(time);
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+  const features = featureCollection.features.filter((feature) => {
+    const featureDate = new Date(feature.properties.time);
+    return (
+      featureDate.getFullYear() === year &&
+      featureDate.getMonth() === month &&
+      featureDate.getDate() === day
+    );
+  });
+  return { type: "FeatureCollection", features };
+}
+
+export default function MapboxComponent() {
+  const [allDays, useAllDays] = useState(true);
+  const [timeRange, setTimeRange] = useState([0, 0]);
+  const [selectedTime, selectTime] = useState(0);
+  const [earthquakes, setEarthQuakes] = useState(null);
+
+  useEffect(() => {
+    fetch("https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson")
+      .then((resp) => resp.json())
+      .then((json) => {
+        // Note: In a real application you would do a validation of JSON data before doing anything with it,
+        // but for demonstration purposes we ingore this part here and just trying to select needed data...
+        const features = json.features;
+        const endTime = features[0].properties.time;
+        const startTime = features[features.length - 1].properties.time;
+
+        setTimeRange([startTime, endTime]);
+        setEarthQuakes(json);
+        selectTime(endTime);
+      })
+      .catch((err) => console.error("Could not load data", err)); // eslint-disable-line
+  }, []);
+
+  const data = useMemo(() => {
+    return allDays
+      ? earthquakes
+      : filterFeaturesByDay(earthquakes, selectedTime);
+  }, [earthquakes, allDays, selectedTime]);
+
+  return (
+    <div>
+      <div className="h-[50rem]">
+        <MapGL
+          initialViewState={{
+            latitude: 40,
+            longitude: -100,
+            zoom: 3,
+          }}
+          projection="globe"
+          fog={{
+            color: "rgba(255,254,254,0.25)",
+            "horizon-blend": 0.001,
+            // "space-color": "#000000",
+          }}
+          mapStyle="mapbox://styles/mapbox/dark-v10"
+          mapboxAccessToken={MAPBOX_TOKEN}
+        >
+          {data && (
+            <Source type="geojson" data={data}>
+              <Layer {...heatmapLayer} />
+              <Layer {...circleLayer} />
+            </Source>
+          )}
+        </MapGL>
+      </div>
+      <ControlPanel
+        startTime={timeRange[0]}
+        endTime={timeRange[1]}
+        selectedTime={selectedTime}
+        allDays={allDays}
+        onChangeTime={selectTime}
+        onChangeAllDays={useAllDays}
+      />
+    </div>
+  );
+}
+
+// export function renderToDom(container) {
+//   createRoot(container).render(<App />);
+// }
